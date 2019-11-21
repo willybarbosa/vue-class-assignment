ef0a3168b5a2aafc4f4177be84840133188df8f9
willybarbosa <57968637+willybarbosa@users.noreply.github.com>
2019-11-21 17:57:36 +0200

    home page

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">ALOHA..OLA.BONGU.ALLO.WELCOME!!</h1>
+      <p class="lead"> Barbosa College scoreboard </p>
+      <hr class="my-4">
+      <p>Sign up and keep track of all activities for both parents and teachers at the click of a button</p>
+      <router-link class="btn btn-primary btn-lg" to="/signup" role="button">Sign up</router-link>
+    </div>
+  </div>
+</template>ls
+
+<script>
+export default {
+  filterResutl(){
+
+  }
+}
+</script>
